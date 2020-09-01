7d7558d977c22fac07c09650931569742a5555b0
Tikaryan <aryantikarya9@gmail.com>
2020-09-01 11:34:57 +0530

    changes in server

--- main.go
+++ main.go
@@ -4,10 +4,12 @@ import (
 	"fmt"
 	gojsonrpc "github.com/filecoin-project/go-jsonrpc"
 	"github.com/gorilla/mux"
+	"github.com/urfave/cli/v2"
 	conn "gojsonrpc/DBConnection"
 	"log"
 	"net/http"
 	"net/http/httptest"
+	"os"
 	"strings"
 )
 type RPC struct {
@@ -43,12 +45,36 @@ func (R *RPC) DeleteALl()error{
 	}
 	return nil
 }
-func (R *RPC) DeleteByKey(){
-
-}
+	var port string
+var DbFlag = false
+var ptr = &DbFlag
 func main() {
-	var gorpc = &RPC{}
+	app := cli.NewApp()
+	app.Name = "GO_JSONRPC_CLI_BADGER"
+	app.Authors = []*cli.Author{
+		{Name: "Tikaryan",Email: "aryantikarya9@gmail.com"},
+	}
 
+	// Receiving port number to run the server at that port
+	app.Flags = []cli.Flag{
+		&cli.StringFlag{Destination: &port, Name: "port",Usage: "To get the specific ports which needs to run"},
+	}
+
+	//Calling server based on command
+	app.Commands = []*cli.Command{
+		&cli.Command{Name: "start",Aliases: []string{"st"},Action: serverStart,Description: "running server"},
+	}
+	err := app.Run(os.Args)
+	if err != nil{
+		log.Fatalln(err)
+	}
+
+	if *ptr {
+	defer conn.CloseDB()
+	}
+}
+func serverStart(ctx *cli.Context)error{
+	var gorpc = &RPC{}
 	rpcServer := gojsonrpc.NewServer()
 	rpcServer.Register("GORPC", gorpc)
 
@@ -57,9 +83,13 @@ func main() {
 	fmt.Println("**starting-server***")
 	route.Handle("/", rpcServer)
 	httptest.NewServer(route)
-	err := http.ListenAndServe(":8080",route)
-	if err != nil{
-		log.Fatal("error listening",err)
+
+	//COnnect DB
+	ptr = conn.DbConnect(ptr)
+	err := http.ListenAndServe(":"+port,route)
+
+	if err != nil {
+		return err
 	}
-	defer conn.CloseDB()
+	return nil
 }
